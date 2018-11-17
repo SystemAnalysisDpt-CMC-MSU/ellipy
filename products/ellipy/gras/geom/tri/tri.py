4079352260bebdf326febd768fc57b5ed7c06dad
Jacob Grigoriev <jacobgrigoriev1997@gmail.com>
2018-11-17 16:43:12 +0000

    issue #6: the function icosahedron() in tri.py is implemented

--- products/ellipy/gras/geom/tri/tri.py
+++ products/ellipy/gras/geom/tri/tri.py
@@ -11,7 +11,27 @@ def ell_tube_discr_tri(n_dim: int, m_dim: int) -> np.ndarray:
 
 
 def icosahedron() -> Tuple[np.ndarray, np.ndarray]:
-    pass
+    IND_VEC = np.array(np.arange(0, 5))
+    Z_VEC = np.array([0.5] * 5)
+    pi = np.pi
+    tau = (np.sqrt(5.0) + 1) / 2
+    r = tau - 0.5
+    vMat = np.ndarray(shape=(12,3), dtype=float, buffer=np.zeros(shape=(12,3)))
+    vMat[0][-1] = 1.0
+    vMat[-1][-1] = -1.0
+    #
+    alphaVec=-pi / 5 + IND_VEC * pi/2.5
+    vMat[2+IND_VEC] = np.column_stack((np.cos(alphaVec)/r, np.sin(alphaVec)/r, Z_VEC/r))
+    #
+    alphaVec=IND_VEC * pi/2.5
+    vMat[7+IND_VEC] = np.column_stack((np.cos(alphaVec)/r, np.sin(alphaVec)/r, -Z_VEC/r))
+    fMat = np.ndarray(shape=(20, 3), dtype=float, buffer=np.array([
+        [1.0, 2, 3], [1, 3, 4],  [1, 4, 5],   [1, 5, 6],    [1, 6, 2],
+        [2, 7, 3],   [3, 8, 4],  [4, 9, 5],   [5, 10, 6],   [6, 11, 2],
+        [7, 8, 3],   [8, 9, 4],  [9, 10, 5],  [10, 11, 6],  [11, 7, 2],
+        [7, 12, 8],  [8, 12, 9], [9, 12, 10], [10, 12, 11], [11, 12, 7]
+    ]))
+    return vMat, fMat
 
 
 def map_face_2_edge(v_mat: np.ndarray, f_mat: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
