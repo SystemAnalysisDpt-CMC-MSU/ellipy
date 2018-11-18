f013c5061243ab8bb4c4f8eae676339057d1439c
olya5698 <o.s.ch@yandex.ru>
2018-11-18 17:48:23 +0300

    issue #9: Some errors and warnings are fixed

--- products/ellipy/gras/la/la.py
+++ products/ellipy/gras/la/la.py
@@ -51,44 +51,40 @@ def ml_orth_transl(src_mat: np.ndarray, dst_arr: np.ndarray) -> np.ndarray:
     dst_arr = dst_arr.reshape((n_dims, n_vecs, n_elems))
     o_arr = np.zeros((n_dims, n_dims, n_elems, n_vecs), dtype=float)
     for i_vec in range(n_vecs):
-        tmp_vec = src_mat[:, i_vec]
-        tmp_vec_len = tmp_vec.shape[0]
-        src_vec = tmp_vec.reshape((tmp_vec_len, 1))
+        src_vec = np.expand_dims(src_mat[:, i_vec], axis=1)
         for i_elem in range(n_elems):
-            tmp_vec = dst_arr[:, i_vec, i_elem]
-            tmp_vec_len = tmp_vec.shape[0]
-            dst_vec = tmp_vec.reshape((tmp_vec_len, 1))
+            dst_vec = np.expand_dims(dst_arr[:, i_vec, i_elem], axis=1)
             o_arr[:, :, i_elem, i_vec] = orth_transl(src_vec, dst_vec)
     return o_arr
 
 
 def orth_transl(src_vec: np.ndarray, dst_vec: np.ndarray) -> np.ndarray:
-    abs_tol = 1e-7
+    __ABS_TOL = 1e-7
     n_dims = dst_vec.shape[0]
     src_vec = try_treat_as_real(src_vec)
     dst_vec = try_treat_as_real(dst_vec)
     dst_squared_norm = np.sum(dst_vec * dst_vec)
     src_squared_norm = np.sum(src_vec * src_vec)
 
-    if dst_squared_norm == 0:
+    if dst_squared_norm == 0.0:
         throw_error('wrongInput:dstZero', 'destination vectors are expected to be non-zero')
-    if src_squared_norm == 0:
+    if src_squared_norm == 0.0:
         throw_error('wrongInput:srcZero', 'source vectors are expected to be non-zero')
 
     dst_vec = dst_vec / np.sqrt(dst_squared_norm)
     src_vec = src_vec / np.sqrt(src_squared_norm)
 
     scal_prod = np.sum(src_vec * dst_vec)
-    s_val = np.sqrt(max(1 - scal_prod * scal_prod, 0))
-    q_mat = np.zeros((n_dims, 2), dtype=float)
-    q_mat[:, 0] = dst_vec[:, 0]
-    if np.abs(s_val) > abs_tol:
-        q_mat[:, 1] = ((src_vec - scal_prod * dst_vec) / s_val)[:, 0]
+    s_val = np.sqrt(max(1.0 - scal_prod * scal_prod, 0.0))
+    q_mat = np.zeros((n_dims, 2), dtype=np.float64)
+    q_mat[:, 0] = np.squeeze(dst_vec)
+    if np.abs(s_val) > __ABS_TOL:
+        q_mat[:, 1] = np.squeeze((src_vec - scal_prod * dst_vec) / s_val)
     else:
-        q_mat[:, 1] = 0
+        q_mat[:, 1] = 0.0
 
-    s_mat = [[scal_prod - 1, s_val], [-s_val, scal_prod - 1]]
-    o_mat = np.identity(n_dims, dtype=float) + np.dot(np.dot(q_mat, s_mat), q_mat.transpose())
+    s_mat = np.array([[scal_prod - 1.0, s_val], [-s_val, scal_prod - 1.0]], dtype=np.float64)
+    o_mat = np.identity(n_dims, dtype=np.float64) + np.dot(np.dot(q_mat, s_mat), q_mat.transpose())
     return o_mat
 
 
@@ -114,13 +110,13 @@ def reg_mat(inp_mat: np.ndarray, reg_tol: float) -> np.ndarray:
 
 
 def reg_pos_def_mat(inp_mat: np.ndarray, reg_tol: float) -> np.ndarray:
-    if not(np.isscalar(reg_tol) and is_numeric(reg_tol) and reg_tol > 0):
+    if not(np.isscalar(reg_tol) and is_numeric(reg_tol) and reg_tol > 0.0):
         throw_error('wrongInput:reg_tol', 'reg_tol must be a positive numeric scalar')
     reg_tol = try_treat_as_real(reg_tol)
     if not(is_mat_symm(inp_mat)):
         throw_error('wrongInput:inp_mat', 'matrix must be symmetric')
     d_mat, v_mat = np.linalg.eig(inp_mat)
-    m_mat = np.diag(np.maximum(0, reg_tol-d_mat))
+    m_mat = np.diag(np.maximum(0.0, reg_tol-d_mat))
     m_mat = np.dot(np.dot(v_mat, m_mat), v_mat.transpose())
     regular_mat = inp_mat + m_mat
     regular_mat = 0.5 * (regular_mat + regular_mat.transpose())
@@ -132,7 +128,7 @@ def sqrtm_pos(q_mat: np.ndarray, abs_tol: float) -> np.ndarray:
 
 
 def try_treat_as_real(inp_mat:  np.ndarray, tol_val: float = np.finfo(float).eps) -> np.ndarray:
-    if not(np.isscalar(tol_val) and is_numeric(tol_val) and tol_val > 0):
+    if not(np.isscalar(tol_val) and is_numeric(tol_val) and tol_val > 0.0):
         throw_error('wrongInput:tol_val', 'tol_val must be a positive numeric scalar')
     if np.all(np.isreal(inp_mat)):
         return inp_mat
@@ -142,6 +138,7 @@ def try_treat_as_real(inp_mat:  np.ndarray, tol_val: float = np.finfo(float).eps
         if norm_value < tol_val:
             return inp_mat.real
         else:
-            out_vec = str('Norm of imaginary part of source object = ' + str(norm_value) +
-                          '. It can not be more than tol_val = ' + str(tol_val))
-            throw_error('wrongInput:inp_mat', out_vec)
+            throw_error('wrongInput:inp_mat',
+                        'Norm of imaginary part of source object = {}. It can not be more then tol_val = {}.'
+                        .format(norm_value, tol_val))
+
