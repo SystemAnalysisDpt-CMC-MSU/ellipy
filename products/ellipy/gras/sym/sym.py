513cc855a1c7e2f5320ec09aa7ae226a48b5facb
votsalevskaya <liubavotsalevskaya@gmail.com>
2018-11-23 21:18:51 +0300

    issue #13 implemented is_dependent function in sym.py tests for is_dependent function

--- products/ellipy/gras/sym/sym.py
+++ products/ellipy/gras/sym/sym.py
@@ -1,8 +1,19 @@
 import numpy as np
+import re
 
 
 def is_dependent(m_mat: np.ndarray, is_discrete: bool = False) -> bool:
-    pass
+    if is_discrete:
+        reg_expression = b'(^k\b|k|^k$|\bk$)'
+    else:
+        reg_expression = b'(^t\b|t|^t$|\bt$)'
+    arr = np.concatenate(m_mat).astype('str')  # realisation of cell2mat
+    reg_arr = re.search(reg_expression, arr, flags=re.MULTILINE)  # = regexp
+    if reg_arr:
+        is_depend = True
+    else:
+        is_depend = False
+    return is_depend
 
 
 def var_replace(m_mat: np.ndarray, from_var_name: str, to_var_name: str) -> np.ndarray:
--- products/ellipy/gras/sym/test/test_sym.py
+++ products/ellipy/gras/sym/test/test_sym.py
@@ -1,2 +1,10 @@
+import numpy as np
+from products.ellipy.gras.sym.sym import is_dependent
+
+
 class TestSym:
-    pass
+    def test_is_dependent(self):
+        assert is_dependent(np.array([['cos(t)', 'sin(t)'], ['-sin(t)', 'cost(t)']], dtype='str'))
+        assert not is_dependent(np.array([['cos(t)', 'sin(t)'], ['-sin(t)', 'cost(t)']], dtype='str'), is_discrete=True)
+        assert is_dependent(np.array([['cos', 'k'], ['-sin', 'cosk']], dtype='str'), is_discrete=True)
+        assert not is_dependent(np.array([['cos', 'k'], ['-sin', 'cosk']], dtype='str'))
