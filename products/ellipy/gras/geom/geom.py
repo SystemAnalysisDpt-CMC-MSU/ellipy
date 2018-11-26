73b572770c5a7b0a9c412e346f4e96cc63e52ea3
Ilya Rublev <irublev@alliedtesting.com>
2018-11-26 22:14:30 +0300

    Function circle_part is made more stable with respect to n_points

--- products/ellipy/gras/geom/geom.py
+++ products/ellipy/gras/geom/geom.py
@@ -6,14 +6,13 @@ def circle_part(n_points: int, return_apart: bool = False,
                 angle_range_vec: np.ndarray = None) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
     if angle_range_vec is None:
         angle_range_vec = np.array([0., 2 * np.pi])
-    d_phi = (angle_range_vec[1] - angle_range_vec[0]) / n_points
-    v_phi = np.r_[angle_range_vec[0] : angle_range_vec[1]: d_phi].reshape(-1,1)
+    v_phi = np.expand_dims(np.linspace(angle_range_vec[0], angle_range_vec[1], n_points, endpoint=False), 2)
     if return_apart:
-        x_mat = np.cos(v_phi)
+        x_vec = np.cos(v_phi)
         y_vec = np.sin(v_phi)
-        return x_mat, y_vec
-    x_mat = np.concatenate([np.cos(v_phi), np.sin(v_phi)], 1)
-    return x_mat
+        return x_vec, y_vec
+    else:
+        return np.concatenate([np.cos(v_phi), np.sin(v_phi)], 1)
 
 
 def sphere_part(n_points: int) -> np.ndarray:
